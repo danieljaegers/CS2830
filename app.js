7e26b3264c4557e7c343c9bc80b1041c578eb349
Daniel Jaegers <daj6gd@mail.missouri.edu>
2020-03-18 17:16:38 -0500

    Fixed the active class

--- app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".navbar_item a").each(function() {
-        if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
-            $(this).parent().addClass('active');
-        }
-    });
-  });
