1a7e9adef517cde4cd1892d2fde04f185a2f81a0
Daniel Jaegers <daj6gd@mail.missouri.edu>
2020-03-18 19:47:40 -0500

    Updated header comments

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// Daniel Jaegers
+// DAJ6GD
+// 3/18/2020
+// Challenge 7: Web Servers S20
+
 function handleResponsive() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
